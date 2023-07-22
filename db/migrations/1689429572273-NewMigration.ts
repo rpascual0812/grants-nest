ba52912dbcd39de7c2128e7a775427b7468d1a3f
Rafael Pascual <raffi@smartpadpro.com.au>
2023-07-22 10:44:14 +0800

    Added admin account migration

--- db/migrations/1689429572273-NewMigration.ts
+++ db/migrations/1689429572273-NewMigration.ts
@@ -3,9 +3,14 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class NewMigration1689429572273 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            insert into accounts (username, password, verified) values ('admin', '$2b$10$zQaTm9vfET4odd0nQsLei.Tgke45X.wf5YVLDncznno3FbaMEUKLi', true);
+        `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            TRUNCATE TABLE accounts RESTART IDENTITY;
+        `);
     }
-
 }
