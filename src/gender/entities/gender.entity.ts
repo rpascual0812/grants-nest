3c4ae4c8db77a9aba45442d8fdd7ae7a0ad546ce
Rafael Pascual <rpascual0812@gmail.com>
2023-07-30 08:56:14 +0800

    Added user roles

---
+++ src/gender/entities/gender.entity.ts
@@ -0,0 +1,22 @@
+import { User } from 'src/user/entities/user.entity';
+import { Entity, Column, PrimaryGeneratedColumn, Unique, JoinColumn, ManyToOne, OneToOne, BaseEntity, AfterLoad, OneToMany } from 'typeorm';
+
+@Entity({ name: 'genders' })
+@Unique(['name'])
+export class Gender {
+    @PrimaryGeneratedColumn()
+    pk: number;
+
+    @Column({ type: 'text', nullable: false })
+    name: string;
+
+    @Column({ default: false })
+    archived: boolean;
+
+    /**
+     * Relationship
+     */
+    @OneToMany('User', (user: User) => user.gender)
+    @JoinColumn({ name: 'pk' })
+    user: Array<User>;
+}
