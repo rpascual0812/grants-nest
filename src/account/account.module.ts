fb42173489c41e83027096b67d1a63b5ebf00d9c
Rafael Pascual <rpascual0812@gmail.com>
2023-07-30 16:54:44 +0800

    Added Password reset

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -5,13 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from './entities/account.entity';
 import { User } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
+import { EmailService } from 'src/email/email.service';
+import { Email } from 'src/email/entities/email.entity';
+import { SessionService } from 'src/session/session.service';
+import { Session } from 'src/session/entities/session.entity';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Account, User]),
+        TypeOrmModule.forFeature([Account, User, Email, Session]),
     ],
     controllers: [AccountController],
-    providers: [AccountService, UserService],
+    providers: [AccountService, UserService, EmailService, SessionService],
     exports: [AccountService]
 })
 export class AccountModule { }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,13 +6,17 @@ import { UserController } from './user.controller';
 import { User } from './entities/user.entity';
 import { Account } from 'src/account/entities/account.entity';
 import { AccountService } from 'src/account/account.service';
+import { EmailService } from 'src/email/email.service';
+import { Email } from 'src/email/entities/email.entity';
+import { Session } from 'src/session/entities/session.entity';
+import { SessionService } from 'src/session/session.service';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([User, Account]),
+        TypeOrmModule.forFeature([User, Account, Email, Session]),
     ],
     controllers: [UserController],
-    providers: [UserService, AccountService],
+    providers: [UserService, AccountService, EmailService, SessionService],
     exports: [UserService]
 })
 export class UserModule { }
