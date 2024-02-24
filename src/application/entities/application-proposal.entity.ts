63bc745169087dfa6a74977178d5c8a02fdb08c9
Rafael Pascual <rpascual0812@gmail.com>
2024-02-24 11:39:40 +0800

    Application Organization Profile Proposed Activities and Timeline Application Documents

---
+++ src/application/entities/application-proposal.entity.ts
@@ -0,0 +1,30 @@
+import { Entity, Column, PrimaryGeneratedColumn, Unique, OneToOne, ManyToOne, JoinColumn, BaseEntity, OneToMany } from 'typeorm';
+import { Application } from './application.entity';
+import { ApplicationProposalActivity } from './application-proposal-activity.entity';
+
+@Entity({ name: 'application_proposal' })
+export class ApplicationProposal extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    pk: number;
+
+    @Column({ name: 'application_pk', nullable: false })
+    application_pk: number;
+
+    @Column({ type: 'text', nullable: false })
+    monitor: string;
+
+    @Column({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+    date_created: Date;
+
+    /**
+     * Relationship
+     */
+
+    @OneToOne(type => Application, application => application.application_proposal, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'application_pk' })
+    application: Application;
+
+    @OneToMany('ApplicationProposalActivity', (applicationProposalActivity: ApplicationProposalActivity) => applicationProposalActivity.application_proposal, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+    @JoinColumn({ name: 'pk' })
+    application_proposal_activity: ApplicationProposalActivity;
+}
