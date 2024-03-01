7b80367889bbb0140cd6e415a4fb6ea7ed6173f9
gitgregoryfuntera <gregory.funtera@gmail.com>
2024-03-01 16:12:36 +0800

    [sc-124]: initial commit [sc-124]: exposed partner list

--- src/partner/partner.controller.ts
+++ src/partner/partner.controller.ts
@@ -5,30 +5,30 @@ import { UpdatePartnerDto } from './dto/update-partner.dto';
 
 @Controller('partner')
 export class PartnerController {
-  constructor(private readonly partnerService: PartnerService) {}
+    constructor(private readonly partnerService: PartnerService) {}
 
-  @Post()
-  create(@Body() createPartnerDto: CreatePartnerDto) {
-    return this.partnerService.create(createPartnerDto);
-  }
+    @Post()
+    create(@Body() createPartnerDto: CreatePartnerDto) {
+        return this.partnerService.create(createPartnerDto);
+    }
 
-  @Get()
-  findAll() {
-    return this.partnerService.findAll();
-  }
+    @Get()
+    findAll() {
+        return this.partnerService.findAll();
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.partnerService.findOne(+id);
-  }
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.partnerService.findOne(+id);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePartnerDto: UpdatePartnerDto) {
-    return this.partnerService.update(+id, updatePartnerDto);
-  }
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updatePartnerDto: UpdatePartnerDto) {
+        return this.partnerService.update(+id, updatePartnerDto);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.partnerService.remove(+id);
-  }
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.partnerService.remove(+id);
+    }
 }
