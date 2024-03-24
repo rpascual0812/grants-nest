3f8cbbd9b9a03fba906438b4b91f8005575a28e6
gitgregoryfuntera <gregory.funtera@gmail.com>
2024-03-24 21:37:09 +0800

    [sc-177]: initial partner filter

--- src/partner/partner.controller.ts
+++ src/partner/partner.controller.ts
@@ -1,25 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request, UseInterceptors, UploadedFile, Response, HttpStatus, UnauthorizedException, InternalServerErrorException, Put } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+    UseGuards,
+    Request,
+    UseInterceptors,
+    UploadedFile,
+    Response,
+    HttpStatus,
+    UnauthorizedException,
+    InternalServerErrorException,
+    Put,
+    Query,
+} from '@nestjs/common';
 import { PartnerService } from './partner.service';
 
 @Controller('partner')
 export class PartnerController {
-    constructor(private readonly partnerService: PartnerService) { }
+    constructor(private readonly partnerService: PartnerService) {}
 
     @Get()
-    async fetch() {
-        let partners = await this.partnerService.findAll();
+    async fetch(@Query() query: { organization_pk?: number }) {
+        const { organization_pk } = query;
+        const partners = await this.partnerService.findAll({
+            organization_pk: organization_pk ? +organization_pk : null,
+        });
         // ugly hack to get the last status and count grand total
         if (partners && partners.data) {
-            partners.data.forEach(partner => {
+            partners.data.forEach((partner) => {
                 partner['grand_total_amount'] = 0;
-                partner.application.forEach(application => {
+                partner.application.forEach((application) => {
                     application['application_status'] = null;
                     if (application.application_statuses.length > 0) {
                         const count = application.application_statuses.length;
                         application['application_status'] = application.application_statuses[count - 1];
                     }
                     if (application.application_proposal) {
-                        partner['grand_total_amount'] += parseInt(application.application_proposal.budget_request_usd.toString());
+                        partner['grand_total_amount'] += parseInt(
+                            application.application_proposal.budget_request_usd.toString(),
+                        );
                     }
                     delete application.application_statuses;
                 });
