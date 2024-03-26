d12a6b5b73fec7e799cc6e542254be03b340cbd9
gitgregoryfuntera <gregory.funtera@gmail.com>
2024-03-26 08:07:45 +0800

    [sc-186]: initial commit

--- src/type/entities/type.entity.ts
+++ src/type/entities/type.entity.ts
@@ -1,8 +1,21 @@
 import { ApplicationStatus } from 'src/application/entities/application-statuses.entity';
+import { Application } from 'src/application/entities/application.entity';
 import { ProjectStatus } from 'src/projects/entities/project-statuses.entity';
 import { Project } from 'src/projects/entities/project.entity';
 import { User } from 'src/user/entities/user.entity';
-import { Entity, Column, PrimaryGeneratedColumn, Unique, OneToOne, ManyToOne, JoinColumn, BaseEntity, OneToMany } from 'typeorm';
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    Unique,
+    OneToOne,
+    ManyToOne,
+    JoinColumn,
+    BaseEntity,
+    OneToMany,
+    ManyToMany,
+    JoinTable,
+} from 'typeorm';
 
 @Entity({ name: 'types' })
 @Unique(['name'])
@@ -29,8 +42,21 @@ export class Type extends BaseEntity {
      * Relationship
      */
 
+    @ManyToOne((type) => User, (user) => user.application, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+    @JoinColumn({ name: 'created_by' })
+    user: User;
+
+    @ManyToMany(() => Application, (application) => application.types)
+    @JoinTable({
+        name: 'type_application_relation',
+        joinColumn: {
+            name: 'type_pk',
+            referencedColumnName: 'pk',
+        },
+    })
+    applications: Application[];
+
     @OneToMany('Project', (project: Project) => project.type)
     @JoinColumn({ name: 'pk' })
     project: Array<Project>;
 }
-
