0c78916edc5715a04fb2f56cc675ee77fdb381b2
Rafael Pascual <rpascual0812@gmail.com>
2024-04-21 08:21:03 +0800

    Grants Module Query Update

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Project } from './entities/project.entity';
+import { Email } from 'src/email/entities/email.entity';
 
 @Module({
-  controllers: [ProjectsController],
-  providers: [ProjectsService]
+    imports: [TypeOrmModule.forFeature([Project, Email])],
+    controllers: [ProjectsController],
+    providers: [ProjectsService]
 })
-export class ProjectsModule {}
+export class ProjectsModule { }
